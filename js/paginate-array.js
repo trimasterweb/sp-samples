d73536c836879c6cd74a29201eac089332cb7542
Jonas Buriti <jonas_cot_05@hotmail.com>
2018-04-11 17:30:47 -0300

    Update paginate-array.js

--- js/paginate-array.js
+++ js/paginate-array.js
@@ -1,10 +1,8 @@
 function paginateArray (array, itemsPerPage){
   return array.reduce(function(pairs, item, index){
-      var multiplo = index + 1;
-      multiplo = multiplo + (multiplo%itemsPerPage)
-      currentPair = (pairs[multiplo] || []).concat(item)
-      pairs[multiplo] = currentPair
+      var group = Math.ceil((index+1)/itemsPerPage)	  
+      currentPair = (pairs[group] || []).concat(item)
+      pairs[group] = currentPair
       return pairs
   },{})
 }
- 
