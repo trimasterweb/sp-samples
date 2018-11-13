7176f83afe83990d7f09ef0ffa0d4c5054077cb8
Luiz Costa <lhcosta.rato@outlook.com>
2018-11-13 14:45:37 -0200

    ajuste de var faltando em currentPair

--- js/paginate-array.js
+++ js/paginate-array.js
@@ -1,7 +1,7 @@
 function paginateArray (array, itemsPerPage){
   return array.reduce(function(pairs, item, index){
-      var group = Math.ceil((index+1)/itemsPerPage)	  
-      currentPair = (pairs[group] || []).concat(item)
+      var group = Math.ceil((index+1)/itemsPerPage),
+          currentPair = (pairs[group] || []).concat(item);
       pairs[group] = currentPair
       return pairs
   },{})
